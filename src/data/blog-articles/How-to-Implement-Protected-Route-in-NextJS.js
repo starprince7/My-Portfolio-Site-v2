46e69e86ad2ecb8bd6c55e4145ff3eb88d696644
starprince7 <princeagezinweke@gmail.com>
2023-08-14 19:49:53 +0100

    blog post: Article 3 - Implementing Protected Routes in Next.js: A Step-by-Step Guide with Code Examples

---
+++ src/data/blog-articles/How-to-Implement-Protected-Route-in-NextJS.js
@@ -0,0 +1,20 @@
+const title =
+  "Implementing Protected Routes in Next.js: A Step-by-Step Guide with Code Examples";
+const snippet =
+  "Preventing access to specific routes in many web applications is essential to making sure that only users with valid credentials may access them. Next.js, a popular React framework, makes it simple to construct secured routes by combining components and authentication logic. I'll walk you through the process of implementing protected routes in a Next.js application, with code examples, in this article.";
+const slug = "How-to-Implement-Protected-Route-in-NextJS";
+
+const Article3 = {
+  Id: 3,
+  slug: slug,
+  articleDate: "August 14, 2023",
+  keyWords: ["React.js", "Next.js", "Javascript", "Frontend", "Software development"],
+  articleUrl: `/blog/${slug}`,
+  articleImage:
+    // "https://images.unsplash.com/photo-1485460535564-844461f37f77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGVhcm4lMjBjb2Rpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1635602739175-bab409a6e94c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2676&q=80",
+  articleTitle: title,
+  articleSnippet: snippet,
+};
+
+export default Article3;
--- src/data/blog-articles/index.js
+++ src/data/blog-articles/index.js
@@ -1,7 +1,9 @@
 import Article1 from './Undoing-a-Git-Pull'
 import Article2 from './How-to-Call-a-Function-Inside-a-React-Component'
+import Article3 from './How-to-Implement-Protected-Route-in-NextJS'
 
 export default [
     Article1,
-    Article2
+    Article2,
+    Article3,
 ]
