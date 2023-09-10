55d21b16331bc628af9b503019738dd4833ef9cd
starprince7 <princeagezinweke@gmail.com>
2023-09-10 18:05:55 +0100

    refactor: Cleanup at /api/notify.js

--- src/pages/api/notify.js
+++ src/pages/api/notify.js
@@ -1,35 +1,24 @@
-const Mailgun = require('mailgun.js')
-const formData = require('form-data')
-// Intialize Mailgun
-const mailgun = new Mailgun(formData)
-const DOMAIN = process.env.MAILGUN_ACCEPTED_DOMAIN;
-
-const auth_credentials = {
-    username: 'api',
-    key: process.env.MAILGUN_API_KEY,
-    url: 'https://api.mailgun.net'
-}
+import mailer from "../../services/mail-service";
 
 const notificationHandler = async (req, res) => {
-    const { name, email, message } = req.body;
+  const { name, email, message } = req.body;
 
-    const client = mailgun.client(auth_credentials)
+  const mail_options = {
+    from: "Portfolio Site <codeplugservices@gmail.com>",
+    to: process.env.ADMIN_EMAIL,
+    subject: "Get In Touch",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
-    const mail_options = {
-        from: "Portfolio Site <codeplugservices@gmail.com>",
-        to: process.env.ADMIN_EMAIL,
-        subject: "Get In Touch",
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
-
-    try {
-        const mail_success = await client.messages.create(DOMAIN, mail_options)
-        mail_success && console.log("MESSAGE SENT!!!");
-        mail_success && res.status(200).json({ msg: "Message has been delivered successfully."});
-    } catch (e) {
-        console.log("ERROR OCCURED!!! =====", e);
-        res.status(401).json({ error: e.message })
-    }
+  try {
+    const mail_success = await mailer(mail_options);
+    mail_success && console.log("MESSAGE SENT!!!");
+    mail_success &&
+      res.status(200).json({ msg: "Message has been delivered successfully." });
+  } catch (e) {
+    console.log("ERROR OCCURED!!! =====", e);
+    res.status(401).json({ error: e.message });
+  }
 };
 
-export default notificationHandler
+export default notificationHandler;
