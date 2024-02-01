41f8dc0badf86e2dc6b0f84dd51b2404e91ef1f3
Prince Agezi Nweke <princeagezinweke@gmail.com>
2024-02-01 11:07:35 +0100

    chore: Blog article threads-API

--- src/data/blog-articles/index.js
+++ src/data/blog-articles/index.js
@@ -1,9 +1,11 @@
 import Article1 from './Undoing-a-Git-Pull'
 import Article2 from './How-to-Call-a-Function-Inside-a-React-Component'
 import Article3 from './How-to-Implement-Protected-Route-in-NextJS'
+import Article4 from './threads-api.js'
 
 export default [
     Article1,
     Article2,
     Article3,
+    Article4,
 ]
---
+++ src/data/blog-articles/threads-api.js
@@ -0,0 +1,19 @@
+const title =
+  "Head of Instagram says Threads API is being developed";
+const snippet =
+  "Adam Mosseri, the head of Instagram, announced today that a Threads API is being developed. Developers will then have the opportunity to craft unique applications and experiences centered around Threads.";
+const slug = "threads-api";
+
+const Article4 = {
+  Id: 4,
+  slug: slug,
+  articleDate: "Febuary 1, 2024",
+  keyWords: ["threads", "threads api", "api", "instagram", "React.js", "Next.js", "Javascript", "Frontend", "Software development"],
+  articleUrl: `/blog/${slug}`,
+  articleImage:
+    "https://techcrunch.com/wp-content/uploads/2023/08/threads-desktop-GettyImages-1524135861.jpg?resize=1200,800",
+  articleTitle: title,
+  articleSnippet: snippet,
+};
+
+export default Article4;
