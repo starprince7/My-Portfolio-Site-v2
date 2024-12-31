ee7419e11697708b8f0c6bbae3e8ffa6ccfe12f7
starprince7 <princeagezinweke@gmail.com>
2024-12-31 18:54:37 +0100

    feat: DeleteUserAccountGuidePage

---
+++ src/pages/vehicle/user-account-delete-guide.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function DeleteUserAccountGuidePage() {
+  return (
+    <>
+      <h1>Step by step guide to delete your account</h1>
+      <ol>
+        <li>Go to account</li>
+        <li>Click delete</li>
+      </ol>
+      <p>That's it your account will be deleted immediately.</p>
+    </>
+  );
+}
